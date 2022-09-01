356a0dc2b2b39bcc4375b69b1393a3ed5e1da3d6
nat <capitaine.nat54230@gmail.com>
2022-09-01 21:02:38 +0200

    add modification on the screen, upgrade of arrow

--- public/js/swichImage.js
+++ public/js/swichImage.js
@@ -1,23 +1,60 @@
-function imgJWB() {  
-    img.classList.remove(oulaoula);
-
-
-    // toggle_btn.addEventListener('click', function(){
-
-    //     if (btn.classList.contains(InversButton)) {
-    //         btn.classList.remove(InversButton);
-    //     }
-    //     else
-    //     {
-    //         btn.classList.add(InversButton); 
-    //     }
-    // });
-}  
-
 //image
 const img = document.getElementById("imgJWB");
 
 // detection des flèches
-const FlècheD = document.getElementById("d");
-const FlècheG = document.getElementById("g");
+const d1 = document.getElementById("d1");
+const g1 = document.getElementById("g1");
+const d2 = document.getElementById("d2");
+const g2 = document.getElementById("g2");
+const d3 = document.getElementById("d3");
+const g3 = document.getElementById("g3");
+const d4 = document.getElementById("d4");
+const g4 = document.getElementById("g4");
+const d5 = document.getElementById("d5");
+const g5 = document.getElementById("g5");
+const d6 = document.getElementById("d6");
+const g6 = document.getElementById("g6");
 
+
+
+
+
+function imgJWB() {  
+    d1.addEventListener ("click", () =>{
+        console.log('flèche droite1 clické')
+    })
+    g1.addEventListener ("click", () =>{
+        console.log('flèche gauche1 clické')
+    })
+    d2.addEventListener ("click", () =>{
+        console.log('flèche droite2 clické')
+    })
+    g2.addEventListener ("click", () =>{
+        console.log('flèche gauche2 clické')
+    })
+    d3.addEventListener ("click", () =>{
+        console.log('flèche droite3 clické')
+    })
+    g3.addEventListener ("click", () =>{
+        console.log('flèche gauche3 clické')
+    })
+    d4.addEventListener ("click", () =>{
+        console.log('flèche droite4 clické')
+    })
+    g4.addEventListener ("click", () =>{
+        console.log('flèche gauche4 clické')
+    })
+    d5.addEventListener ("click", () =>{
+        console.log('flèche droite5 clické')
+    })
+    g5.addEventListener ("click", () =>{
+        console.log('flèche gauche5 clické')
+    })
+    d6.addEventListener ("click", () =>{
+        console.log('flèche droite6 clické')
+    })
+    g6.addEventListener ("click", () =>{
+        console.log('flèche gauche6 clické')
+    })
+
+}  
