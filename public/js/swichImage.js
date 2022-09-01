0e02a2ba66334979823b2e58fa8208a75bfd3968
nat <capitaine.nat54230@gmail.com>
2022-09-01 14:22:20 +0200

    add modification on cursor and other

---
+++ public/js/swichImage.js
@@ -0,0 +1,23 @@
+function imgJWB() {  
+    img.classList.remove(oulaoula);
+
+
+    // toggle_btn.addEventListener('click', function(){
+
+    //     if (btn.classList.contains(InversButton)) {
+    //         btn.classList.remove(InversButton);
+    //     }
+    //     else
+    //     {
+    //         btn.classList.add(InversButton); 
+    //     }
+    // });
+}  
+
+//image
+const img = document.getElementById("imgJWB");
+
+// detection des flèches
+const FlècheD = document.getElementById("d");
+const FlècheG = document.getElementById("g");
+
