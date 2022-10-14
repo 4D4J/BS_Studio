74dd48b69f45003cdf944fd3599438fc38182909
nat <capitaine.nat54230@gmail.com>
2022-10-15 00:43:04 +0200

    add opacity backgound after click btn + add btn <Next Step> for buy, correction of micro detail on a code, optimisasion

--- public/js/buy.js
+++ public/js/buy.js
@@ -3,15 +3,26 @@ const div = document.getElementById('alert')
 const bg = document.getElementById('bgOver')
 
 
-
-
 btn.addEventListener("click", () => {
     console.log("clické")
     if(getComputedStyle(div).display != "none"){
         div.style.display = "none";
-        bg.style.display = "none";        
+        bg.style.display = "none"; 
     }else{
-        div.style.display = "block";
-        bg.style.display = "block"; 
+        div.style.display = "flex";
+        bg.style.display = "flex"; 
+    }
+})
+
+
+
+bg.addEventListener("click", () =>{
+    console.log("background cliqué")
+    if(getComputedStyle(div).display != "flex"){
+        div.style.display = "flex";
+        bg.style.display = "flex";   
+    }else{
+        div.style.display = "none";
+        bg.style.display = "none"; 
     }
 })
