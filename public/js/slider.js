30c21f5962672cee9d8c5da51e1d17b0992207c2
Nàt <82668500+4D4J@users.noreply.github.com>
2024-10-18 19:21:05 +0200

    Debug du slider pour l'image 1 et 2 --> doit etre optimiser

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,22 +1,57 @@
-let currentImageIndex = 0;
-const images = document.querySelectorAll('.slider-img');
 
-function showImage(index) {
-    images.forEach((img, i) => {
+// compteur d'image, sert à changer l'image par la suivante 
+let currentImage_1_Index = 0;
+let currentImage_2_Index = 0;
+
+// permet de selectionner la classe à modifier 
+const images_1 = document.querySelectorAll('.slider-img_1');
+const images_2 = document.querySelectorAll('.slider-img_2');
+
+
+
+
+// slider image 1
+function showImage_1(index) {
+    images_1.forEach((img, i) => {
         if (i === index) {
+            img.classList.remove('hidden')
             img.classList.add('active');
         } else {
             img.classList.remove('active');
+            img.classList.add('hidden')
         }
     });
 }
-
-function nextImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    showImage(currentImageIndex);
+// changer l'image 1
+function nextImage_1() {
+    currentImage_1_Index = (currentImage_1_Index + 1) % images_1.length;
+    showImage_1(currentImage_1_Index);
+}
+function prevImage_1() {
+    currentImage_1_Index = (currentImage_1_Index - 1 + images_1.length) % images_1.length;
+    showImage_1(currentImage_1_Index);
 }
 
-function prevImage() {
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    showImage(currentImageIndex);
+
+
+// slider image 2
+function showImage_2(index) {
+    images_2.forEach((img, i) => {
+        if (i === index) {
+            img.classList.remove('hidden')
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+            img.classList.add('hidden')
+        }
+    });
+}
+// changer l'image 2
+function nextImage_2() {
+    currentImage_2_Index = (currentImage_2_Index + 1) % images_2.length;
+    showImage_2(currentImage_2_Index);
+}
+function prevImage_2() {
+    currentImage_2_Index = (currentImage_2_Index - 1 + images_2.length) % images_2.length;
+    showImage_2(currentImage_2_Index);
 }
